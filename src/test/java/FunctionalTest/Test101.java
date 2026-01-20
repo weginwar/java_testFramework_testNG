c558b85c22d773f34d8a30765d65a0ce9839ee54
gweginwar <gweginwar@gmail.com>
2026-01-20 18:41:30 +0530

    initial commit

---
+++ src/test/java/FunctionalTest/Test101.java
@@ -0,0 +1,48 @@
+package FunctionalTest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test101 {
+	
+	
+	@Test(groups= {"smoke","regression"})
+	public static void test01() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
+		
+		driver.get("https://demoqa.com/");
+		driver.quit();
+		
+	}
+	
+	@Test(groups= {"smoke","regression"})
+	public static void test02() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
+		
+		driver.get("https://demoqa.com/");
+		driver.quit();
+		
+	}
+	@Test(groups= {"smoke","regression"})
+	public static void test03() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
+		
+		driver.get("https://demoqa.com/");
+		driver.quit();
+		
+	}
+
+	
+}
